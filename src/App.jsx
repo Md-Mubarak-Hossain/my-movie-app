d39fd95e045b16186dcf8fcc61d198a26c2b9774
Md-Mubarak-Hossain <hossainmubarak477@gmail.com>
2023-01-21 17:37:09 +0600

    movie added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import './App.css';
 import unirest from 'unirest';
-
+import Movie from "./components/Movie.jsx";
+import Search from './components/Search.jsx';
 class App extends React.Component {
-
+ 
  sendRequest = (title) => {
    const req = unirest("GET", "https://movie-database-imdb-alternative.p.rapidapi.com/");
 
@@ -15,25 +16,35 @@ class App extends React.Component {
 
    req.headers({
      "x-rapidapi-host": "movie-database-imdb-alternative.p.rapidapi.com",
-     "x-rapidapi-key": "YOUR_API_KEY"
+     "x-rapidapi-key": "a7cfa4ef05msh8cec39513c262bfp11f466jsn82a5d2d5c838"
+     
    });
 
-
+   state = {
+    movies: []
+  }
    req.end((res) => {
-     if (res.error) throw new Error(res.error);
-
-     console.log(res.body);
-   });
+    if (res.error) throw new Error(res.error);
+    const movies = res.body.Search;
+    this.setState({movies});
+  });
  }
 
  render() {
-   return (
-     <div className="App">
+   return (<div className="App">
        <header className="App-header">
+       {
+       this.state.movies.map((movie) => {
+     return <Movie {...movie}/>
+   })
+  }
+      <Search handleSendRequest={this.sendRequest}/>
        </header>
      </div>
    );
  }
+ 
+
 }
 
 export default App;
